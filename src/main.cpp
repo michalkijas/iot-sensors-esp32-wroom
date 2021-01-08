262ee0a0e7681a039812840c7adc667540aabb65
Michał Kijas <kijasmichal@gmail.com>
2021-01-08 16:26:05 +0100

    Added reading temperature and humidity from sensor DHT22

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,32 @@
 #include <Arduino.h>
+#include "DHT.h"
+
+#define DHT_PIN 17
+#define DHT_TYPE DHT22
+DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
     Serial.begin(115200);
     Serial.println("Setup");
+    dht.begin();
 }
 
 void loop() {
-    Serial.println("Hello world!");
-    delay(1000);
+    delay(2000);
+
+    float humidity = dht.readHumidity();
+    float temperatureCelsius = dht.readTemperature();
+    float temperatureFahrenheit = dht.readTemperature(true);
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(temperatureCelsius) || isnan(temperatureFahrenheit)) {
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return;
+    }
+
+    Serial.printf("Humidity: %f %%, Temperature: %f °C %f °F\n", humidity, temperatureCelsius, temperatureFahrenheit);
+
+    float heatIndexCelsius = dht.computeHeatIndex(temperatureCelsius, humidity, false);
+    float heatIndexFahrenheit = dht.computeHeatIndex(temperatureFahrenheit, humidity);
+    Serial.printf("Heat index: %f °C %f °F\n", heatIndexCelsius, heatIndexFahrenheit);
 }
